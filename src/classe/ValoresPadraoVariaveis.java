402bddcd50e69fc28e7d64a33ec5cf6da91c9126
m-arc <m-arc@192.168.1.8>
2022-04-19 09:00:00 -0300

    Atualização

---
+++ src/classe/ValoresPadraoVariaveis.java
@@ -0,0 +1,15 @@
+package classe;
+
+public class ValoresPadraoVariaveis {
+
+	byte a = 0;// Valor Padrão
+	short b = 0;// Valor Padrão
+	int c = 0;// Valor Padrão
+	long d = 0;// Valor Padrão
+	float e = 0;// Valor Padrão
+	double f = 0.0;// Valor Padrão
+	boolean g = false;// Valor Padrão
+	char h = 0;// Valor Padrão
+	String i = null;// Valor Padrão "Não aponta para nenhum local de memoria
+
+}
