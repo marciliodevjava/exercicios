58f8658f1a426e70548b0f4a6594458012e630c9
m-arc <m-arc@192.168.1.8>
2022-04-20 18:06:34 -0300

    Atualização

--- src/classe/EqualsHashcode.java
+++ src/classe/EqualsHashcode.java
@@ -3,8 +3,8 @@ package classe;
 public class EqualsHashcode {
 
 	public static void main(String[] args) {
-		
-		//Utilizando o Equals e Hashcode
+
+		// Utilizando o Equals e Hashcode
 		Usuario u1 = new Usuario();
 		u1.nome = "Pedro Silva";
 		u1.email = "pedro.silva@ezemail.com.br";
@@ -12,12 +12,13 @@ public class EqualsHashcode {
 		Usuario u2 = new Usuario();
 		u2.nome = "Pedro Silva";
 		u2.email = "pedro.silva@ezemail.com.br";
-		
+
 		System.out.println(u1 == u2);
 		System.out.println(u1.equals(u2));
 		System.out.println(u2.equals(u1));
-		
+
 		System.out.println(u1 == u1);
 		System.out.println(u2 == u2);
+
 	}
 }
